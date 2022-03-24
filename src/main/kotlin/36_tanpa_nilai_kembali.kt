a2c1837cf4fc656ae194d1743d5bd10419d68eae
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-24 21:01:26 +0700

    Percobaan Fungsi Lambda Tanpa Nilai Kembali

---
+++ src/main/kotlin/36_tanpa_nilai_kembali.kt
@@ -0,0 +1,12 @@
+fun main() {
+    //Fungsi lambda tanpa nilai kembali
+    displayMessage("Rekayasa Perangkat Lunak", 10);
+}
+
+fun displayMessage(msg: String, count: Int) {
+    var counter = 1
+    while(counter <= count ) {
+        println("$msg $counter")
+        counter++
+    }
+}
