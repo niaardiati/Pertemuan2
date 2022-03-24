17f8557592b6d1575f15cf80ec6ebb356d80ef76
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-24 21:15:27 +0700

    Percobaan Ekspresi Lambdas

---
+++ src/main/kotlin/41_ekspresi_lambdas.kt
@@ -0,0 +1,22 @@
+fun main() {
+    //Ekspresi lambda selalu dikelilingi oleh tanda blok „{}‟ deklarasi parameter berada dalam blok yang memiliki opsional type annotation pada bagian tubuh blok ditandai dengan tanda „->‟. 
+    println(MyFunction("Rekayasa Perangkat Lunak"))
+
+    //Lambda Function
+    val mylambda :(String)->Unit = {s:String->print(s)}
+    val v:String = "Jurusan Teknik Informatika"
+    mylambda(v)
+
+    //Inline Function
+    myFun(v,mylambda) //passing lambda as a parameter of another function
+}
+
+fun MyFunction(x: String): String {
+    var c:String = "Hey!! Welcome To ---"
+    return (c+x)
+}
+
+fun myFun(a:String, action:(String)->Unit) { //passing lambda
+    print("\nHeyyy!!!")
+    action(a)// call to lambda function
+}
