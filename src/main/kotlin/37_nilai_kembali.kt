747a27988586fcd127259bdbd46c06a53a30a758
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-24 21:03:26 +0700

    Percobaan Fungsi Lambda dengan Nilai Kembali

---
+++ src/main/kotlin/37_nilai_kembali.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //Fungsi lambda dengan nilai kembali
+    val listNumber = listOf(1,2,3,4,5,6)
+    println("Jumlah nilai pada elemen listNumber = ${getSum(listNumber)}")
+}
+
+fun getSum(values: List<Int>) : Int { // return type is Int
+    var total = 0;
+    for (i in values) total += i
+    return total // return value
+}
