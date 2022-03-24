9663f2f410cd1cc12549fd953569d26da507b2be
Nia Ardiati <ardiatinia01@gmail.com>
2022-03-24 20:53:20 +0700

    Percobaan Menghitung Luas dan Keliling Lingkaran

---
+++ src/main/kotlin/34_luas_dan_keliling_lingkaran.kt
@@ -0,0 +1,14 @@
+import kotlin.math.PI
+
+fun main() {
+    //Advanced exercise
+    print("Enter the circle's radius (cm): ")
+    val radius = readLine()!!.toDouble()
+    //Masukkan rumus Keliling Lingkaran
+    var circumference = 2 * PI * radius
+    //Masukkan rumus Luas Lingkaran
+    var area = PI * radius * radius
+    println("The circle's circumference based on the given radius is: " + circumference + " cm")
+    println("Area of the circle is: " + area + " cm^2")
+
+}
